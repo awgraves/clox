39d0671f2ec9e12be2e6080d1ae46a86ce73db95
Andrew Graves <agraves@codecademy.com>
2024-10-09 14:02:53 -0400

    variable assignment

--- debug.c
+++ debug.c
@@ -50,6 +50,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 			return constantInstruction("OP_GET_GLOBAL", chunk, offset);
 		case OP_DEFINE_GLOBAL:
 			return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
+		case OP_SET_GLOBAL:
+			return constantInstruction("OP_SET_GLOBAL", chunk, offset);
 		case OP_ADD:
 			return simpleInstruction("OP_ADD", offset);
 		case OP_GREATER:
