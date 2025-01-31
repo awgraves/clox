ab0c0189730192b539a3fa2294708864714318e1
Andrew Graves <agraves@codecademy.com>
2025-01-30 20:09:57 -0500

    compile to closures

--- vm.h
+++ vm.h
@@ -2,40 +2,40 @@
 #define clox_vm_h
 
 #include "chunk.h"
+#include "object.h"
 #include "table.h"
 #include "value.h"
-#include "object.h"
 
 #define FRAMES_MAX 64
 #define STACK_MAX (FRAMES_MAX * UINT8_COUNT)
 
 typedef struct {
-	ObjFunction* function;
-	uint8_t* ip;
-	Value* slots;
+  ObjClosure *closure;
+  uint8_t *ip;
+  Value *slots;
 } CallFrame;
 
 typedef struct {
-	CallFrame frames[FRAMES_MAX];
-	int frameCount;
-	Value stack[STACK_MAX];
-	Value* stackTop;
-	Table globals;
-	Table strings;
-	Obj* objects;
+  CallFrame frames[FRAMES_MAX];
+  int frameCount;
+  Value stack[STACK_MAX];
+  Value *stackTop;
+  Table globals;
+  Table strings;
+  Obj *objects;
 } VM;
 
 typedef enum {
-	INTERPRET_OK,
-	INTERPRET_COMPILE_ERROR,
-	INTERPRET_RUNTIME_ERROR
+  INTERPRET_OK,
+  INTERPRET_COMPILE_ERROR,
+  INTERPRET_RUNTIME_ERROR
 } InterpretResult;
 
 extern VM vm;
 
 void initVM();
 void freeVM();
-InterpretResult interpret(const char* source);
+InterpretResult interpret(const char *source);
 void push(Value value);
 Value pop();
 
