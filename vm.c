25681609da2cbca9b64c8ae6c6c3eea028a04d58
Andrew Graves <agraves@codecademy.com>
2025-04-09 15:51:24 -0400

    add class initializers

--- vm.c
+++ vm.c
@@ -64,12 +64,17 @@ void initVM() {
   initTable(&vm.globals);
   initTable(&vm.strings);
 
+  vm.initString =
+      NULL; // zero out the value before interning in case the GC runs
+  vm.initString = copyString("init", 4); // intern the string for performance
+
   defineNative("clock", clockNative);
 }
 
 void freeVM() {
   freeTable(&vm.globals);
   freeTable(&vm.strings);
+  vm.initString = NULL; // free the interned string
   freeObjects();
 }
 
@@ -115,6 +120,13 @@ static bool callValue(Value callee, int argCount) {
     case OBJ_CLASS: {
       ObjClass *klass = AS_CLASS(callee);
       vm.stackTop[-argCount - 1] = OBJ_VAL(newInstance(klass));
+      Value initializer;
+      if (tableGet(&klass->methods, vm.initString, &initializer)) {
+        return call(AS_CLOSURE(initializer), argCount);
+      } else if (argCount != 0) {
+        runtimeError("Expected 0 arguments but got %d.", argCount);
+        return false;
+      }
       return true;
     }
     case OBJ_CLOSURE:
